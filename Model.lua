5e345c28a94002913c237b4177161cf0011a8c78
Zappaman <iosifadrianmihai@gmail.com>
2016-03-22 23:06:40 +0200

    added memory-only model, added sigmoid in probability, seems to be in (0,1) now

--- Model.lua
+++ Model.lua
@@ -9,9 +9,19 @@ local Model = {}
 
 
 --------------------------------------------------------------------------------
--- Creates model
+-- Creates model, may use external input besides
 --------------------------------------------------------------------------------
 function Model.create(opt)
+    if opt.noInput ~= nil then
+        return Model.__createNoInput(opt)
+    end
+    return Model.__createWithInput(opt)
+end
+
+--------------------------------------------------------------------------------
+-- Creates model with separate input and memory
+--------------------------------------------------------------------------------
+function Model.__createWithInput(opt)
     local vectorSize = tonumber(opt.vectorSize)
     local memSize = tonumber(opt.memorySize)
     local inputSize = tonumber(opt.inputSize)
@@ -21,7 +31,6 @@ function Model.create(opt)
     ----------------------------------------------------------------------------
     local initialMem = nn.Identity()()
     ----------------------------------------------------------------------------
-
 
     ----------------------------------------------------------------------------
     -- Input
@@ -42,7 +51,7 @@ function Model.create(opt)
     ----------------------------------------------------------------------------
     -- Value Extractor
     ----------------------------------------------------------------------------
-    --TODO for current problems may not need to send value here
+    
     local addressTransp = nn.Reshape(1, memSize)(address)
     local value = nn.MM()({addressTransp, initialMem})
     ----------------------------------------------------------------------------
@@ -64,7 +73,6 @@ function Model.create(opt)
     local valueCalc =
         nn.GRU(inputSize + memSize + vectorSize, vectorSize)(inputValAddr)
     ----------------------------------------------------------------------------
-
 
     ----------------------------------------------------------------------------
     -- Memory Calculator
@@ -95,11 +103,105 @@ function Model.create(opt)
     local allInOne = nn.JoinTable(1)({addrValCalc, reshapedMem})
     local h1 = nn.Linear(vectorSize + memSize + memSize * vectorSize, 10)
         (allInOne)
-    local p = nn.Linear(10, 1)(nn.Sigmoid()(nn.Linear(10, 10)(nn.Sigmoid()(h1))))
+
+    local p = nn.Sigmoid()(nn.Linear(10, 1)(nn.Sigmoid()(nn.Linear(10, 10)(
+        nn.Sigmoid()(h1)))))
     ----------------------------------------------------------------------------
 
     return nn.gModule({initialMem, input}, {finMem, p})
 
 end
+
+
+--------------------------------------------------------------------------------
+-- Creates model based only on memory
+--------------------------------------------------------------------------------
+function Model.__createNoInput(opt)
+    local vectorSize = tonumber(opt.vectorSize)
+    local memSize = tonumber(opt.memorySize)
+
+    ----------------------------------------------------------------------------
+    --  Initial Memory
+    ----------------------------------------------------------------------------
+    local initialMem = nn.Identity()()
+    ----------------------------------------------------------------------------
+
+
+    ----------------------------------------------------------------------------
+    --  Address Encoder
+    ----------------------------------------------------------------------------
+    local reshapedMem = nn.Reshape(memSize * vectorSize)(initialMem)
+    local enc = nn.GRU(memSize * vectorSize, memSize)(reshapedMem)
+    local address = nn.SoftMax()(enc)
+    ----------------------------------------------------------------------------
+
+
+    ----------------------------------------------------------------------------
+    -- Value Extractor
+    ----------------------------------------------------------------------------
+    --TODO for current problems may not need to send value here
+    local addressTransp = nn.Reshape(1, memSize)(address)
+    --return nn.gModule({initialMem}, {addressTransp})
+
+    local value = nn.MM()({addressTransp, initialMem})
+    ------------------------------------------------------------------------------
+
+    ------------------------------------------------------------------------------
+    ---- Next address calculator
+    ------------------------------------------------------------------------------
+    local reshapedValue = nn.Squeeze(1)(value)
+    local valAddr = nn.JoinTable(1)({address, reshapedValue})
+    local addrCalc =
+        nn.GRU(memSize + vectorSize, memSize)(valAddr)
+    ------------------------------------------------------------------------------
+
+
+    ------------------------------------------------------------------------------
+    ---- Next value calculator
+    ------------------------------------------------------------------------------
+    local valueCalc =
+        nn.GRU(memSize + vectorSize, vectorSize)(valAddr)
+    ------------------------------------------------------------------------------
+
+
+    ------------------------------------------------------------------------------
+    ---- Memory Calculator
+    ------------------------------------------------------------------------------
+
+    ----adder
+    local resizeValueCalc = nn.Reshape(1, vectorSize)(valueCalc)
+    local resizeAddrCalc = nn.Reshape(memSize, 1)(addrCalc)
+    local adder = nn.MM()({resizeAddrCalc, resizeValueCalc})
+
+
+    ---- eraser
+    local addrCalcTransp = nn.Reshape(1, memSize)(addrCalc)
+    local AT_M_t_1 =  nn.MM()({addrCalcTransp, initialMem})
+    local resAddrCalc = nn.Reshape(memSize, 1)(addrCalc)
+    local AAT_M_t_1 = nn.MM()({resAddrCalc, AT_M_t_1})
+
+    ----memory update
+    local memEraser = nn.CSubTable()({initialMem, AAT_M_t_1})
+    local finMem = nn.CAddTable()({memEraser, adder})
+    ------------------------------------------------------------------------------
+
+
+    ------------------------------------------------------------------------------
+    ---- Probability calculator
+    ------------------------------------------------------------------------------
+    local addrValCalc = nn.JoinTable(1)({addrCalc, valueCalc})
+    local allInOne = nn.JoinTable(1)({addrValCalc, reshapedMem})
+    local h1 = nn.Linear(vectorSize + memSize + memSize * vectorSize, 10)
+        (allInOne)
+
+    local p = nn.Sigmoid()(nn.Linear(10, 1)(nn.Sigmoid()(nn.Linear(10, 10)(
+        nn.Sigmoid()(h1)))))
+    ------------------------------------------------------------------------------
+
+    return nn.gModule({initialMem}, {finMem, p})
+
+end
+
+
 
 return Model
--- ModelUnitTests.lua
+++ ModelUnitTests.lua
@@ -21,7 +21,7 @@ Model = require("Model")
 -- Model test definitions
 --------------------------------------------------------------------------------
 modelTests = {}
-modelTests["sanityCheck"] = function()
+modelTests["inputAndMemory"] = function()
     ----------------------------------------------------------------------------
     -- Dummy dataset
     ----------------------------------------------------------------------------
@@ -71,6 +71,68 @@ modelTests["sanityCheck"] = function()
     m = Model.create(opt)
     memory = Tensor(opt.memorySize, opt.vectorSize):fill(0)
     local f = m:forward({memory, dataset:getNextBatch(1)[1][1][1]})
+
+    --check sizes of forward pass to see if they're what we expect
+    if f[1]:isSameSizeAs(memory) and f[2]:isSameSizeAs(Tensor(1))
+    then
+        return "...OK!"
+    else
+        return "...fail!"
+    end
+
+end
+
+
+modelTests["memory"] = function()
+    ----------------------------------------------------------------------------
+    -- Dummy dataset
+    ----------------------------------------------------------------------------
+    local cmd = torch.CmdLine()
+    cmd:text()
+    cmd:text('Generate datasets for learning algorithms')
+    cmd:text()
+    cmd:text('Options')
+    cmd:option('-dataFile','train.t7', 'filename of the training set')
+    cmd:option('-vectorSize', 5, 'size of single training instance vector')
+    cmd:option('-trainSize', 80, 'size of training set')
+    cmd:option('-testSize', 50, 'size of test set')
+    cmd:option('-datasetType', 'repeat_binary', 'dataset type')
+    cmd:option('-minVal', 1, 'minimum scalar value of dataset instances')
+    cmd:option('-maxVal', 300, 'maximum scalar value of dataset instances')
+    cmd:option('-memorySize', 500, 'number of entries in memory')
+    cmd:text()
+
+    local opt = cmd:parse(arg)
+
+    dataset = Dataset.create(opt)
+    ----------------------------------------------------------------------------
+    -- Dummy command line options
+    ----------------------------------------------------------------------------
+    cmd = torch.CmdLine()
+    cmd:text()
+    cmd:text('Training a neural architecture to learn algorithms')
+    cmd:text()
+    cmd:text('Options')
+    cmd:option('-trainFile','train.t7', 'filename of the training set')
+    cmd:option('-testFile', 'test.t7', 'filename of the test set')
+    cmd:option('-batchSize', '16', 'number of sequences to train in parallel')
+    cmd:text()
+    opt = cmd:parse(arg)
+
+    ----------------------------------------------------------------------------
+    -- Run sanity check on size
+    ----------------------------------------------------------------------------
+
+    setmetatable(dataset, Dataset)
+    dataset:resetBatchIndex()
+
+    opt.vectorSize = dataset.vectorSize
+    opt.memorySize = dataset.memorySize
+    opt.inputSize = dataset.inputSize
+    opt.noInput = true
+    m = Model.create(opt)
+    memory = Tensor(opt.memorySize, opt.vectorSize):fill(0)
+    local f = m:forward(memory)
 
     --check sizes of forward pass to see if they're what we expect
     if f[1]:isSameSizeAs(memory) and f[2]:isSameSizeAs(Tensor(1))
